d170bb3f7369014d7f7551eab5433ac7d66149f9
Re'em Bensimhon <reem@forter.com>
2017-10-06 12:50:34 +0300

    Jar shading and placeholder main func

---
+++ src/main/kotlin/org/oryx/kumulus/Main.kt
@@ -0,0 +1,9 @@
+package org.oryx.kumulus
+
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
+fun main(args: Array<String>) {
+    logger.info { "This is a placeholder for a future main method implementation" }
+}
