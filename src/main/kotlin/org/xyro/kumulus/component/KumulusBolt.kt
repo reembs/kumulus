27652739466cac54998e47a884cd92b989f20e27
Yoav  Schwartz <yoav.schwartz@forter.com>
2025-11-12 11:25:14 +0200

    compy component changes

---
+++ src/main/kotlin/org/xyro/kumulus/component/KumulusBolt.kt
@@ -0,0 +1,40 @@
+package org.xyro.kumulus.component
+
+import mu.KotlinLogging
+import org.apache.storm.generated.ComponentCommon
+import org.apache.storm.generated.GlobalStreamId
+import org.apache.storm.generated.Grouping
+import org.apache.storm.task.OutputCollector
+import org.apache.storm.task.TopologyContext
+import org.apache.storm.topology.IRichBolt
+import org.xyro.kumulus.KumulusTuple
+import org.xyro.kumulus.collector.KumulusBoltCollector
+
+class KumulusBolt(
+    config: Map<String, Any>,
+    context: TopologyContext,
+    componentInstance: IRichBolt,
+    common: ComponentCommon?
+) : KumulusComponent(config, context) {
+    companion object {
+        private val logger = KotlinLogging.logger {}
+    }
+
+    val inputs: Map<GlobalStreamId, Grouping> = common?._inputs?.toMap() ?: mapOf()
+    val streams = common?._streams?.toMap() ?: mapOf()
+
+    var tickSecs: Number? = null
+
+    private val bolt: IRichBolt = componentInstance
+
+    fun prepare(collector: KumulusBoltCollector) {
+        logger.info { "Created bolt '$componentId' with taskId $taskId (index: ${context.thisTaskIndex}). Object hashcode: ${this.hashCode()}" }
+        bolt.prepare(config, context, OutputCollector(collector))
+        super.prepare()
+    }
+
+    fun execute(tuple: KumulusTuple) {
+        logger.debug { "Executing bolt '$componentId' with taskId $taskId (index: ${context.thisTaskIndex}). Input: ${tuple.kTuple}" }
+        bolt.execute(tuple.kTuple)
+    }
+}
---
+++ src/main/kotlin/org/xyro/kumulus/component/KumulusSpout.kt
@@ -0,0 +1,121 @@
+package org.xyro.kumulus.component
+
+import mu.KotlinLogging
+import org.apache.storm.spout.SpoutOutputCollector
+import org.apache.storm.task.TopologyContext
+import org.apache.storm.topology.IRichSpout
+import org.xyro.kumulus.KumulusAcker
+import org.xyro.kumulus.KumulusTopology
+import org.xyro.kumulus.collector.KumulusSpoutCollector
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.atomic.AtomicBoolean
+
+class KumulusSpout(
+    config: Map<String, Any>,
+    context: TopologyContext,
+    componentInstance: IRichSpout
+) : KumulusComponent(config, context) {
+    companion object {
+        private val logger = KotlinLogging.logger {}
+    }
+
+    val spout: IRichSpout = componentInstance
+
+    private val deactivationLock = Any()
+    private val deactivated = AtomicBoolean(false)
+
+    val queue = LinkedBlockingQueue<AckMessage>()
+
+    fun prepare(collector: KumulusSpoutCollector) {
+        logger.debug { "Created spout '$componentId' with taskId $taskId (index: ${context.thisTaskIndex}). Object hashcode: ${this.hashCode()}" }
+        spout.open(config, context, SpoutOutputCollector(collector))
+        super.prepare()
+    }
+
+    private fun nextTuple() {
+        spout.nextTuple()
+    }
+
+    private fun ack(msgId: Any?) {
+        spout.ack(msgId)
+    }
+
+    private fun fail(
+        msgId: Any?,
+        timeoutComponents: List<String>,
+        failedComponents: List<String>
+    ) {
+        if (spout is KumulusTimeoutNotificationSpout) {
+            spout.messageIdFailure(msgId, failedComponents, timeoutComponents)
+        } else if (spout is KumulusFailureNotificationSpout) {
+            spout.messageIdFailure(msgId, failedComponents + timeoutComponents)
+        }
+        spout.fail(msgId)
+    }
+
+    fun activate() {
+        spout.activate()
+    }
+
+    fun deactivate() {
+        if (!deactivated.get()) {
+            synchronized(deactivationLock) {
+                if (!deactivated.get()) {
+                    deactivated.set(true)
+                    spout.deactivate()
+                }
+            }
+        }
+    }
+
+    fun start(topology: KumulusTopology) {
+        Thread {
+            try {
+                while (true) {
+                    if (isReady.get()) {
+                        activate()
+                        break
+                    }
+                    Thread.sleep(topology.readyPollSleepTime)
+                }
+                while (true) {
+                    mainLoopMethod(topology.acker)
+                    if (!isReady.get()) {
+                        spout.deactivate()
+                        return@Thread
+                    }
+                }
+            } catch (e: Exception) {
+                logger.error("An uncaught exception in spout '$componentId' (taskId: $taskId) had forced a Kumulus shutdown", e)
+                topology.throwException(e)
+            }
+        }.apply {
+            isDaemon = true
+            start()
+        }
+    }
+
+    private fun mainLoopMethod(acker: KumulusAcker) {
+        queue.poll()?.also { ackMessage ->
+            if (ackMessage.ack) {
+                ack(ackMessage.spoutMessageId)
+            } else {
+                fail(ackMessage.spoutMessageId, ackMessage.timeoutComponents, ackMessage.failedComponents)
+            }
+        }.let {
+            if (it == null && isReady.get()) {
+                if (acker.waitForSpoutAvailability()) {
+                    if (inUse.compareAndSet(false, true)) {
+                        try {
+                            if (isReady.get()) {
+                                nextTuple()
+                            }
+                        } finally {
+                            inUse.set(false)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
